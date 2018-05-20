fa84efdf91c738a8361c6a4289d46ac0d0c87b8a
ameer <ameer@ameers-macbook-pro.local>
2018-05-20 14:35:27 +0300

    second commit

---
+++ Sllti/Sllti/Sllti/Controller/Sllti/ItemesViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ItemesViewController.swift
+//  Sllti
+//
+//  Created by ameer on 5/20/18.
+//  Copyright © 2018 Novent App. All rights reserved.
+//
+
+import UIKit
+
+class ItemesViewController: UIViewController ,UICollectionViewDelegate , UICollectionViewDataSource {
+    @IBOutlet weak var itemsCoolectionView: UICollectionView!
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = itemsCoolectionView.dequeueReusableCell(withReuseIdentifier: "ItemsCell", for: indexPath) as! ItemsCollectionViewCell
+        cell.imgeItems.image = UIImage(named: "lion")
+        return cell
+    }
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+//    {
+//        return CGSize(width: 1000, height: 800.0)
+//    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       itemsCoolectionView.delegate = self
+        itemsCoolectionView.dataSource = self
+
+       
+    }
+
+}
